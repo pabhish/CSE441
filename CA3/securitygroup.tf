f5a15b7c17398ba4d867e17fe242378bddb2e85e
Aditya Raj <56469932+th3-v3ng34nc3@users.noreply.github.com>
2023-04-21 15:06:06 +0530

    Create securitygroup.tf

---
+++ CA3/securitygroup.tf
@@ -0,0 +1,26 @@
+# Create a security group for the EC2 instance
+resource "aws_security_group" "security_group" {
+  name_prefix = "sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
